5d519b1002d2218ba872a1c39094ac53b3cc67e4
Scott Ouellette <scottx611x@gmail.com>
2020-09-11 11:57:00 -0400

    fixes from failing plan

--- destination_bucket/vars.tf
+++ destination_bucket/vars.tf
@@ -3,12 +3,6 @@ variable "dest_region" {
   description = "AWS region for the destination bucket"
 }
 
-variable "replicate_prefix" {
-  type        = "string"
-  description = "Prefix to replicate, default \"\" for all objects. Note if specifying, must end in a /"
-  default     = ""
-}
-
 variable "dest_bucket_name" {
   type        = "string"
   description = "Name for dest s3 bucket"
@@ -18,9 +12,4 @@ variable "create_dest_bucket" {
   type        = "string"
   description = "Boolean for whether this module should create the destination bucket"
   default     = "true"
-}
-
-variable "replication_name" {
-  type        = "string"
-  description = "Short name to describe this replication"
 }
--- source_bucket/vars.tf
+++ source_bucket/vars.tf
@@ -7,3 +7,14 @@ variable "source_bucket_name" {
   type        = "string"
   description = "Name for source s3 bucket"
 }
+
+variable "replicate_prefix" {
+  type        = "string"
+  description = "Prefix to replicate, default \"\" for all objects. Note if specifying, must end in a /"
+  default     = ""
+}
+
+variable "replication_name" {
+  type        = "string"
+  description = "Short name to describe this replication"
+}
